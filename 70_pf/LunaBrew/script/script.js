7efbb84a73ba2b2ebff6e024074dfabef4cc6213
OceanFlavorStarCandy <rladmswl001214@gmail.com>
2024-08-01 14:13:00 +0900

    0801

--- 70_pf/LunaBrew/script/script.js
+++ 70_pf/LunaBrew/script/script.js
@@ -1,24 +1,36 @@
-// section2 tab
-const btn1 = document.querySelector("#btn1");
-const btn2 = document.querySelector("#btn2");
-const btn3 = document.querySelector("#btn3");
+window.addEventListener("DOMContentLoaded",function(){
+    // section2 tab
+    const btn1 = document.querySelector("#btn1");
+    const btn2 = document.querySelector("#btn2");
+    const btn3 = document.querySelector("#btn3");
 
-const tab1 = document.querySelector("#T1");
-const tab2 = document.querySelector("#T2");
-const tab3 = document.querySelector("#T3");
+    const tab1 = document.querySelector("#T1");
+    const tab2 = document.querySelector("#T2");
+    const tab3 = document.querySelector("#T3");
 
-btn1.onclick = function(){
-    tab1.classList.add('on');
-    tab2.classList.remove('on');
-    tab3.classList.remove('on');
-}
-btn2.onclick = function(){
-    tab1.classList.remove('on');
-    tab2.classList.add('on');
-    tab3.classList.remove('on');
-}
-btn3.onclick = function(){
-    tab1.classList.remove('on');
-    tab2.classList.remove('on');
-    tab3.classList.add('on');
-}
+    btn1.onclick = function(){
+        tab1.classList.add('on');
+        tab2.classList.remove('on');
+        tab3.classList.remove('on');
+    }
+    btn2.onclick = function(){
+        tab1.classList.remove('on');
+        tab2.classList.add('on');
+        tab3.classList.remove('on');
+    }
+    btn3.onclick = function(){
+        tab1.classList.remove('on');
+        tab2.classList.remove('on');
+        tab3.classList.add('on');
+    }
+});
+
+$(function(){
+    $('#wrap').fullpage({
+        //menu: '#hana_nav',
+        //anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+        navigation: true,
+        navigationTooltips: ['Main', 'Company', 'Services', 'Gallery', 'Question'],
+        //showActiveTooltip: true
+    });
+});
--- 70_pf/team2/04. js/sub/menu.js
+++ 70_pf/team2/04. js/sub/menu.js
@@ -1,24 +1,47 @@
-var menuPageB01 = document.getElementById("menu_pageButton1");
-var menuPageB02 = document.querySelector("#menu_pageButton2");
-var menuPageB03 = document.querySelector("#menu_pageButton3");
+window.addEventListener("DOMContentLoaded", function(){
+    const menuPageB01 = document.getElementById("menu_pageButton1");
+    const menuPageB02 = document.querySelector("#menu_pageButton2");
+    const menuPageB03 = document.querySelector("#menu_pageButton3");
 
-const menupage1 = document.querySelector("#menu_page1");
-const menupage2 = document.querySelector("#menu_page2");
-const menupage3 = document.querySelector("#menu_page3");
+    const menupage1 = document.querySelector("#menu_page1");
+    const menupage2 = document.querySelector("#menu_page2");
+    const menupage3 = document.querySelector("#menu_page3");
 
-if (menuPageB01.checked) {
-    console.log("page1");
-    menupage1.style.display = "block";
-    menupage2.style.display = "none";
-    menupage3.style.display = "none";
-} else if (menuPageB02.checked) {
-    console.log("page2");
-    menupage1.style.display = "none";
-    menupage2.style.display = "block";
-    menupage3.style.display = "none";
-} else {
-    console.log("page3");
-    menupage1.style.display = "none";
-    menupage2.style.display = "none";
-    menupage3.style.display = "block";
-}
+    /* if (menuPageB01.checked) {
+        console.log("page1");
+        menupage1.style.display = "block";
+        menupage2.style.display = "none";
+        menupage3.style.display = "none";
+    } else if (menuPageB02.checked) {
+        console.log("page2");
+        menupage1.style.display = "none";
+        menupage2.style.display = "block";
+        menupage3.style.display = "none";
+    } else {
+        console.log("page3");
+        menupage1.style.display = "none";
+        menupage2.style.display = "none";
+        menupage3.style.display = "block";
+    } */
+
+    menuPageB01.onclick = function(){
+        console.log("page1");
+        menupage1.style.display = "block";
+        menupage2.style.display = "none";
+        menupage3.style.display = "none";
+    };
+
+    menuPageB02.onclick = function(){
+        console.log("page2");
+        menupage1.style.display = "none";
+        menupage2.style.display = "block";
+        menupage3.style.display = "none";
+    };
+
+    menuPageB03.onclick = function(){
+        console.log("page3");
+        menupage1.style.display = "none";
+        menupage2.style.display = "none";
+        menupage3.style.display = "block";
+    };
+});
