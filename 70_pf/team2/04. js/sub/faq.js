ee39b50665bfc5f7bf1814c645fb76aeaafa73ce
OceanFlavorStarCandy <rladmswl001214@gmail.com>
2024-08-01 18:02:31 +0900

    0801

---
+++ 70_pf/team2/04. js/sub/faq.js
@@ -0,0 +1,19 @@
+window.addEventListener("DOMContentLoaded", function(){
+    const faqPageB01 = document.querySelector("#faq_pageButton1");
+    const faqPageB02 = document.querySelector("#faq_pageButton2");
+
+    const menupage1 = document.querySelector("#faq_page1");
+    const menupage2 = document.querySelector("#faq_page2");
+
+    faqPageB01.onclick = function(){
+        console.log("page1");
+        menupage1.style.display = "block";
+        menupage2.style.display = "none";
+    };
+
+    faqPageB02.onclick = function(){
+        console.log("page2");
+        menupage1.style.display = "none";
+        menupage2.style.display = "block";
+    };
+});
