e6e97a2ca24830ef257c9376289af971a32072d8
OceanFlavorStarCandy <rladmswl001214@gmail.com>
2024-08-26 17:56:53 +0900

    0826

--- 70_pf/team2/04. js/sub/faq.js
+++ 70_pf/team2/04. js/sub/faq.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", function(){
+    // 페이지
     const faqPageB01 = document.querySelector("#faq_pageButton1");
     const faqPageB02 = document.querySelector("#faq_pageButton2");
 
@@ -9,14 +10,22 @@ window.addEventListener("DOMContentLoaded", function(){
     menupage2.style.display = "none";
 
     faqPageB01.onclick = function(){
-        console.log("page1");
         menupage1.style.display = "block";
         menupage2.style.display = "none";
     };
 
     faqPageB02.onclick = function(){
-        console.log("page2");
         menupage1.style.display = "none";
         menupage2.style.display = "block";
     };
+
+    // 박스 열기
+    /* for(i=0; i<=16; i++) {
+        let checkbox = document.querySelector('.FAQlist input[type="checkbox"]')[i];
+        let lock = document.querySelector('.FAQlist label img')[i];
+
+        checkbox.onclick = function(){
+            lock.style.transform = "rotateX('180deg')";
+        };
+    }; */
 });
