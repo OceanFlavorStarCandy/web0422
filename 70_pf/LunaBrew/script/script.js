f39985a947c6eadbb82a96b469ad1d70d3819387
OceanFlavorStarCandy <rladmswl001214@gmail.com>
2024-09-03 15:01:53 +0900

    0903

--- 70_pf/LunaBrew/script/script.js
+++ 70_pf/LunaBrew/script/script.js
@@ -26,17 +26,28 @@ window.addEventListener("DOMContentLoaded",function(){
 
     // scroll event
     const t = document.querySelector(".scrollEvent");
-    const s2scroll = this.document.querySelector("#s2Text div");
     const header = document.querySelector("header");
+    const s2scroll = document.querySelector("#s2Text div");
+    const s3image01 = document.querySelector("#tabImage1");
+    const s3image02 = document.querySelector("#tabImage2");
 
     window.addEventListener("scroll", function(){
         t.innerHTML = Math.floor(pageYOffset);
+
         if(t.innerHTML >= 840) {
             s2scroll.classList.add('active');
             header.classList.add('active');
         } else {
             s2scroll.classList.remove('active');
             header.classList.remove('active');
+        }
+
+        if(t.innerHTML >= 1720) {
+            s3image01.classList.add('activate');
+            s3image02.classList.add('activate');
+        } else {
+            s3image01.classList.remove('activate');
+            s3image02.classList.remove('activate');
         }
     });
 });
